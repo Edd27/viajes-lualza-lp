8fa920a9c9741743fe8b874999e89cd8c379835e
Marcos Lopez <71472051+LPMexcu@users.noreply.github.com>
2024-01-03 14:56:02 -0600

    Change in the contact

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -16,18 +16,44 @@ export default function Contact() {
       ? contactSection?.content['map']?.replaceAll("\\'", '"')
       : '';
 
+  const contactData = contactSection?.content;
+  const email = contactData?.email;
+  const phone = contactData?.phone;
+  const address = contactData?.address;
+
+  console.log(email, phone, address);
   return (
     <Section verticalAlignment='center'>
-      <h2 className='mb-10 text-2xl lg:text-3xl'>
+      <h2 className='mb-10 text-2xl lg:text-3xl px-36 text-center'>
         {contactSection?.title ?? 'Acerca de nosotros'}
       </h2>
-      <div className='w-full border rounded-md overflow-hidden h-[600px] flex flex-col'>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `${iframeMap}`,
-          }}
-          className='h-full w-full'
-        />
+      <div className='flex w-full text-lg lg:text-xl text-center items-center '>
+        <div className='w-1/2 flex flex-col items-center justify-center '>
+          <h2 className='py-2 '>
+            {address?.street} #{address?.number} {address?.city},{' '}
+            {address?.state}
+          </h2>
+          <a
+            className='py-2'
+            href={`tel:${phone}`}
+          >
+            {phone}
+          </a>
+          <a
+            className='py-2'
+            href={`mailto:${email}`}
+          >
+            {email}
+          </a>
+        </div>
+        <div className='w-1/2 border rounded-md overflow-hidden h-[600px] flex flex-col'>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: `${iframeMap}`,
+            }}
+            className='h-full w-full'
+          />
+        </div>
       </div>
     </Section>
   );
