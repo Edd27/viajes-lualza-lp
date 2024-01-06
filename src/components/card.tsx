559cdc203561e23a47fd9ba2fcd76a20057a1ac7
Edgar Benavides <edgarben27@gmail.com>
2024-01-05 21:21:49 -0600

    Refactor Card component to improve readability and styling

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,13 +6,18 @@ interface ICard {
 
 export default function Card({ title, description, image }: ICard) {
   return (
-    <div className=''>
-      <h2>{title}</h2>
-      <p>{description}</p>
+    <div className='lg:max-w-sm bg-card dark:bg-secondary border border-card rounded-lg shadow'>
       <img
+        className='rounded-t-lg'
         src={image}
         alt={`Imagen de ${title}`}
       />
+      <div className='p-5'>
+        <h5 className='mb-2 text-2xl font-bold tracking-tight text-primary'>
+          {title}
+        </h5>
+        <p className='mb-3 font-normal text-pretty'>{description}</p>
+      </div>
     </div>
   );
 }
