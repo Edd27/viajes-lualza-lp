3429330489601020bd573c5f091cd09ea5a0e98b
Edgar Benavides <edgarben27@gmail.com>
2024-01-05 21:09:36 -0600

    Refactor footer component layout and social icons

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,14 +1,7 @@
 import useCompanyData from '@/store/company';
 import useSiteData from '@/store/site';
 import { ICompany, ISite } from '@/type';
-import {
-  Facebook,
-  Instagram,
-  Mail,
-  MapPin,
-  Phone,
-  Twitter,
-} from 'lucide-react';
+import { Facebook, Instagram, Twitter } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 const SOCIAL_ICONS: {
@@ -31,7 +24,7 @@ export default function Footer() {
   return (
     <footer className='bg-primary text-white'>
       <div className='mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8'>
-        <div className='md:flex md:justify-between'>
+        <div className='grid md:grid-cols-2 md:justify-between'>
           <div className='mb-6 md:mb-0 flex flex-col justify-between gap-5'>
             <Link
               to='/'
@@ -48,10 +41,12 @@ export default function Footer() {
             </Link>
             <p className='max-w-xs text-pretty'>{company?.description}</p>
           </div>
-          <div className='flex gap-8 sm:gap-6'>
+          <div className='grid md:grid-cols-2 gap-3 md:gap-6'>
             <div>
-              <h2 className='mb-6 text-sm font-semibold uppercase'>Contacto</h2>
-              <ul className='font-medium'>
+              <h2 className='mb-3 lg:mb-6 text-sm font-bold uppercase'>
+                Contacto
+              </h2>
+              <ul>
                 {company?.phones?.map((phone) => (
                   <li
                     className='mb-4'
@@ -68,10 +63,10 @@ export default function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className='mb-6 text-sm font-semibold uppercase'>
+              <h2 className='mb-3 lg:mb-6 text-sm font-bold uppercase'>
                 Encuentranos
               </h2>
-              <ul className='font-medium'>
+              <ul>
                 {company?.addresses?.map((address) => (
                   <li
                     key={address.id}
@@ -95,7 +90,7 @@ export default function Footer() {
             </div>
           </div>
         </div>
-        <hr className='my-6 sm:mx-auto lg:my-8' />
+        <hr className='my-6 sm:mx-auto lg:my-8 border-white/40' />
         <div className='sm:flex sm:items-center sm:justify-between'>
           <span className='text-sm sm:text-center'>
             © {new Date().getFullYear()}{' '}
@@ -120,91 +115,6 @@ export default function Footer() {
               </a>
             ))}
           </div>
-        </div>
-      </div>
-    </footer>
-  );
-
-  return (
-    <footer className='bg-primary text-white flex flex-col items-center justify-center py-20 px-3 lg:px-24'>
-      <div className='w-full flex md:justify-center md:text-center'>
-        <Link
-          to='/'
-          className='font-bold text-3xl flex items-center gap-x-2'
-        >
-          <img
-            src={site?.logo}
-            alt={`Logo ${site?.title}`}
-            className='size-10'
-          />
-          <span>{company?.name}</span>
-        </Link>
-      </div>
-
-      <div className='flex flex-col md:flex-row gap-10 md:gap-20 mt-20'>
-        <div className='w-full md:w-1/3'>
-          <h2 className='font-bold text-lg mb-2'>Acerca de nosotros</h2>
-          <p className='text-pretty'>{company?.description}</p>
-        </div>
-        <div className='w-full md:w-1/3'>
-          <h2 className='font-bold text-lg mb-2'>Contacto</h2>
-          <section>
-            {company?.phones?.map((phone) => (
-              <a
-                href={`tel:${phone.number}`}
-                key={phone.id}
-                className='underline md:no-underline hover:underline'
-              >
-                <Phone className='size-6' />
-                <span>{phone.number}</span>
-              </a>
-            ))}
-          </section>
-          <section>
-            {company?.emails?.map((email) => (
-              <a
-                href={`mailto:${email.value}`}
-                key={email.id}
-                className='underline md:no-underline hover:underline'
-              >
-                <Mail className='size-6' />
-                <span>{email.value}</span>
-              </a>
-            ))}
-          </section>
-          <section>
-            {company?.addresses?.map((address) => (
-              <a
-                href={`https://www.google.com/maps?q=${address.street}+${address.number}+${address.suburb}+${address.zip}+${address.city}+${address.state}`}
-                target='_blank'
-                rel='noreferrer noopener'
-                key={address.id}
-                className='underline md:no-underline hover:underline'
-              >
-                <MapPin className='size-6' />
-                <span className='w-full'>
-                  {address.street} {address.number}, {address.suburb},{' '}
-                  {address.zip} {address.city}, {address.state}.
-                </span>
-              </a>
-            ))}
-          </section>
-        </div>
-        <div className='w-full md:w-1/3'>
-          <h2 className='font-bold text-lg mb-2'>Redes sociales</h2>
-          <section className='w-full flex items-center'>
-            {company?.socials?.map((social) => (
-              <a
-                className='size-10'
-                target='_blank'
-                rel='noreferrer noopener'
-                href={social.url}
-                key={social.id}
-              >
-                {SOCIAL_ICONS[social.name.toLowerCase()]}
-              </a>
-            ))}
-          </section>
         </div>
       </div>
     </footer>
